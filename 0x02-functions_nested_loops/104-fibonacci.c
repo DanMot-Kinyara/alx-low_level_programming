b496282c6da7f77ed60305ec251d0b9e9e5ce694
DanMot-Kinyara <danmot16.dm@gmail.com>
2023-02-22 14:00:17 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,18 +12,18 @@ int main(void)
 {
 	int j;
 	/*initialise first an second term */
-	long unsigned num1 = 1;
-	long unsigned num2 = 2;
-	long unsigned next_term;
+	unsigned long num1 = 1;
+	unsigned long num2 = 2;
+	unsigned long next_term;
 
 	/*initialise the next term */
 	next_term = num1 + num2;
 	/*print first two numbers of the fibonacci series */
-	printf("Fibonacci sequence: %ld, %ld, " , num1, num2);
+	printf("%ld, %ld, ", num1, num2);
 
 	for (j = 3; j < 98; j++)
 	{
-		printf("%ld, " , next_term);
+		printf("%ld, ", next_term);
 		num1 = num2;
 		num2 = next_term;
 		next_term = num1 + num2;
