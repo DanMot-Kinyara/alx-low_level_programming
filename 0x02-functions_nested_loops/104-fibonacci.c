3646e96f135333e4faf75b8350345981a021023a
DanMot-Kinyara <danmot16.dm@gmail.com>
2023-02-22 13:43:14 -0800

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Start point
+ *
+ * @j, @num1, @num2, @next_term: fibonacci sequence terms
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int j;
+	/*initialise first an second term */
+	int num1 = 1;
+	int num2 = 2;
+
+	/*initialise the next term */
+	next_term = num1 + num2;
+	/*print first two numbers of the fibonacci series */
+	printf("Fibonacci sequence: %d, %d, "num1, num2)
+	for (j = 3; j < 98; j++)
+	{
+		printf("%d, "next_term);
+		num1 = num2;
+		num2 = next_term;
+		next_term = num1 + num2;
+	}
+}
